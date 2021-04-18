f3007cce67980d3427d7b3b1b4d5e76e41926f85
sha ke <82579992+sha-ce@users.noreply.github.com>
2021-04-18 22:07:05 +0900

    Create const.js

---
+++ const.js
@@ -0,0 +1,15 @@
+//
+//定数の定義用js
+//
+
+const GAME_FPS = 1000 / 60 //FPS
+const SCREEN_SIZE_W = 256; //画面のサイズ(横)
+const SCREEN_SIZE_H = 224; //画面のサイズ(縦)
+
+//1画面当たりのブロックの数
+const MAP_SIZE_W = SCREEN_SIZE_W / 16;
+const MAP_SIZE_H = SCREEN_SIZE_H / 16;
+
+//マップデータのブロックの数
+const FIELD_SIZE_W = 256;
+const FIELD_SIZE_H = 14;
